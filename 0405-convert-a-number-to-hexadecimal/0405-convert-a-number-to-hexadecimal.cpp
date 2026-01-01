ac94745a5df1d619042aea430c6fed1f55856ea2
Krishna Chaitanya <64398510+Krrish3398@users.noreply.github.com>
2026-01-01 14:18:22 +0530

    Time: 0 ms (100%), Space: 7.8 MB (68.99%) - LeetHub

---
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string toHex(int num) {
+        if (num == 0) return "0";
+        unsigned int n = num;
+        string hexMap = "0123456789abcdef";
+        string result = "";
+        while(n > 0)
+        {
+            int last4bits = n & 0xf;
+
+            result += hexMap[last4bits];
+
+            n >>= 4; 
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
